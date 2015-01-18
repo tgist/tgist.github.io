bd558f817c246ac99f63bf0178baa66a2b023cfb
Havanna <Havanna@users.noreply.github.com>
2015-01-18 13:05:27 +0800

    Delete config.rb

--- config.rb
+++
@@ -1,6 +0,0 @@
-http_path = "/"
-css_dir = "style/css"
-sass_dir = "style/_sass"
-images_dir = "style/img"
-javascripts_dir = "style/js"
-output_style = :compressed        # :expanded or :nested or :compact or :compressed
