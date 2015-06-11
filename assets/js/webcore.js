4c8a7dbe9256b6c737b3d9e14b5df87c27990e7e
Register <registerdedicated@gmail.com>
2015-06-11 20:09:44 +0800

    update webcore.js

--- assets/js/webcore.js
+++ assets/js/webcore.js
@@ -5,13 +5,6 @@
     $('a[href]').each(function() {
       if (this.href.indexOf(window.location.host) == -1) $(this).attr({target: '_blank' });
     });
-    $('a[href^=#][href!=#]').click(function() {
-      var target = document.getElementById(this.hash.slice(1));
-      if (!target) return;
-      var targetOffset = $(target).offset().top;
-      $('html,body').animate({scrollTop: targetOffset}, 400);
-      return false;
-    });
   });
 
 
